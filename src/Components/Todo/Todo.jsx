bc900c5787bebfa16ff0adbb9686bba438307cfb
Akbarali2003 <100187499+Akbarali2003@users.noreply.github.com>
2022-03-30 10:36:23 +0500

    kabob image added

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import createFragment from 'react-addons-create-fragment'; 
 import './Todo.scss';
 import UserLittleImg2 from '../../Assets/Images/userlittleimg2.jpg';
 import UserLittleImg3 from '../../Assets/Images/userlittleimg3.jpg';
@@ -10,32 +11,25 @@ import RetweetImg from '../../Assets/Images/retweet.svg';
 import LikeImg from '../../Assets/Images/like.svg';
 import ShareImg from '../../Assets/Images/share.svg';
 import StatisticsImg from '../../Assets/Images/statistics.svg';
+import KabobImg from '../../Assets/Images/kabob.jpg';
+
 function HomeTodo() {
 	const [todos, setTodos] = React.useState([
 		{ id: 0, title: 'Code yozdim', isCompleted: false },
 		{ id: 1, title: 'Code yozmadim', isCompleted: true },
 	]);
+	
 	return (
 		<>
 			<section className="todo">
-				<input
-					onKeyUp={(evt) => {
-						const newTodo = {
-							id: todos[todos.length - 1]?.id + 1 || 0,
-							title: evt.target.value,
-							isCompleted: false,
-						};
-						setTodos([newTodo, ...todos]);
-					}}
-					type="text"
-					placeholder="What’s happening"
-				/>
-				<button type="submit">Submit</button>
-
+				
 				<ul className="todo__list">
 					{todos.map((row) => (
-						<li key={row.id}>{row.title}</li>
+						<li key={row.id}>
+							{row.title}
+						</li>
 					))}
+					<hr className="todo__line" />
 					<li className="todo__item">
 						<div className="todo__user">
 							<img
@@ -46,7 +40,7 @@ function HomeTodo() {
 							/>
 							<span className="todo__user--lname">
 								<span className="todo__user--surename">
-									<span className='todo__user--sname'>
+									<span className="todo__user--sname">
 										<p className="todo__user--name">
 											Designsta
 										</p>
@@ -56,34 +50,44 @@ function HomeTodo() {
 									</span>
 									<img src={ThreePoint} alt="ThreePoint" />
 								</span>
-								<p className='todo__user--commit'>
+								<p className="todo__user--commit">
 									Twitterdagi ayol-erkak
 									qarama-qarshiliginglardan o'zinglar
 									zerikmadinglarmi?
 								</p>
-								<ul>
-									<li>
-										<img src={CommentImg} alt="CommentImg" />
-										<p>10</p>
+								<ul className="todo__icon--list">
+									<li className="todo__icon--item">
+										<img
+											src={CommentImg}
+											alt="CommentImg"
+										/>
+										<p className="todo__icon--count">10</p>
 									</li>
-									<li>
-										<img src={RetweetImg} alt="RetweetImg" />
-										<p>1</p>
+									<li className="todo__icon--item">
+										<img
+											src={RetweetImg}
+											alt="RetweetImg"
+										/>
+										<p className="todo__icon--count">1</p>
 									</li>
-									<li>
+									<li className="todo__icon--item">
 										<img src={LikeImg} alt="LikeImg" />
-										<p>8</p>
+										<p className="todo__icon--count">8</p>
 									</li>
-									<li>
+									<li className="todo__icon--item">
 										<img src={ShareImg} alt="ShareImg" />
 									</li>
-									<li>
-										<img src={StatisticsImg} alt="StatisticsImg" />
+									<li className="todo__icon--item">
+										<img
+											src={StatisticsImg}
+											alt="StatisticsImg"
+										/>
 									</li>
 								</ul>
 							</span>
 						</div>
 					</li>
+					<hr className="todo__line" />
 					<li className="todo__item">
 						<div className="todo__user">
 							<img
@@ -94,7 +98,7 @@ function HomeTodo() {
 							/>
 							<span className="todo__user--lname">
 								<span className="todo__user--surename">
-									<span className='todo__user--sname'>
+									<span className="todo__user--sname">
 										<p className="todo__user--name">
 											cloutexhibition
 										</p>
@@ -104,15 +108,45 @@ function HomeTodo() {
 									</span>
 									<img src={ThreePoint} alt="ThreePoint" />
 								</span>
-								<p className='todo__user--commit'>
+								<p className="todo__user--commit">
 									YPIP dasturining bu yilgi sezoni ham o’z
 									nihoyasiga yetmoqda. Mentorlik davomida
 									talaba va yangi bitiruvchilarni
 									o’sayotganini ko’rib hursand bo’ladi odam.
 								</p>
+								<ul className="todo__icon--list">
+									<li className="todo__icon--item">
+										<img
+											src={CommentImg}
+											alt="CommentImg"
+										/>
+										<p className="todo__icon--count"></p>
+									</li>
+									<li className="todo__icon--item">
+										<img
+											src={RetweetImg}
+											alt="RetweetImg"
+										/>
+										<p className="todo__icon--count">5</p>
+									</li>
+									<li className="todo__icon--item">
+										<img src={LikeImg} alt="LikeImg" />
+										<p className="todo__icon--count">9</p>
+									</li>
+									<li className="todo__icon--item">
+										<img src={ShareImg} alt="ShareImg" />
+									</li>
+									<li className="todo__icon--item">
+										<img
+											src={StatisticsImg}
+											alt="StatisticsImg"
+										/>
+									</li>
+								</ul>
 							</span>
 						</div>
 					</li>
+					<hr className="todo__line" />
 					<li className="todo__item">
 						<div className="todo__user">
 							<img
@@ -123,7 +157,7 @@ function HomeTodo() {
 							/>
 							<span className="todo__user--lname">
 								<span className="todo__user--surename">
-									<span className='todo__user--sname'>
+									<span className="todo__user--sname">
 										<p className="todo__user--name">
 											CreativePhoto
 										</p>
@@ -133,12 +167,48 @@ function HomeTodo() {
 									</span>
 									<img src={ThreePoint} alt="ThreePoint" />
 								</span>
-								<p className='todo__user--commit'>
+								<p className="todo__user--commit">
 									Обетда..... <br /> Кечиринглар
 								</p>
+								<img
+									src={KabobImg}
+									alt="KabobImg"
+									width="679"
+									height="453"
+								/>
+								<ul className="todo__icon--list">
+									<li className="todo__icon--item">
+										<img
+											src={CommentImg}
+											alt="CommentImg"
+										/>
+										<p className="todo__icon--count">10</p>
+									</li>
+									<li className="todo__icon--item">
+										<img
+											src={RetweetImg}
+											alt="RetweetImg"
+										/>
+										<p className="todo__icon--count">1</p>
+									</li>
+									<li className="todo__icon--item">
+										<img src={LikeImg} alt="LikeImg" />
+										<p className="todo__icon--count">8</p>
+									</li>
+									<li className="todo__icon--item">
+										<img src={ShareImg} alt="ShareImg" />
+									</li>
+									<li className="todo__icon--item">
+										<img
+											src={StatisticsImg}
+											alt="StatisticsImg"
+										/>
+									</li>
+								</ul>
 							</span>
 						</div>
 					</li>
+					<hr className="todo__line" />
 				</ul>
 			</section>
 		</>
